fb5b3651cd42a66e62ef1ca94fca67036c8da7b5
2900706877 <2900706877@qq.com>
2026-04-25 16:40:51 +0800

    Add files via upload

--- ipfs.js
+++ ipfs.js
@@ -179,7 +179,7 @@ class IPFSIntegration {
             
             // 尝试从IPFS网关获取内容
             try {
-                const gatewayUrl = `https://lavender-patient-canidae-805.mypinata.cloud/${cid}`;
+                const gatewayUrl = `https://gateway.pinata.cloud/ipfs/${cid}`;
                 const response = await fetch(gatewayUrl);
                 
                 if (response.ok) {
